28638d071aa8879a6f826b43e170d56926d0bedc
Victorcasas <Victorcasas@users.noreply.github.com>
2023-04-08 12:10:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ console.log(location.pathname);
 // Customize title
 function customizeTitle() {
   var x = document.getElementsByTagName("H1")[0]; 
-  x.innerHTML = "El Blog de";  
+  x.innerHTML = "El Blog de";
+  x.style.background = "";
   var y = document.createElement("H1"); 
   var t = document.createTextNode("Víctor Casas San Miguel");
+  y.style.color = "rgb(175, 73, 30)";
   y.appendChild(t);
   var z = document.getElementsByClassName("f2 f1-l b di lh-title mb3 white mw6 bg-primary")[0];
   z.appendChild(y);
