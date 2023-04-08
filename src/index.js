8430f16f2a1b1fbb64e2a865d528afb6f8294e25
Victorcasas <Victorcasas@users.noreply.github.com>
2023-04-08 11:38:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ console.log(location.pathname);
 
 // Customize title
 function customizeTitle() {
-  var x = document.getElementsByTagName("H1")[0]; 
-  x.innerHTML = "El Blog de";  
-  var y = document.createElement("H1"); 
-  var t = document.createTextNode("Víctor Casas San Miguel");
-  y.appendChild(t);
-  var z = document.getElementsByClassName("f2 f1-l b di lh-title mb3 white mw6 bg-primary")[0];
-  z.appendChild(y);
+  if (location.pathname = "/") {
+    var x = document.getElementsByTagName("H1")[0]; 
+    x.innerHTML = "El Blog de";  
+    var y = document.createElement("H1"); 
+    var t = document.createTextNode("Víctor Casas San Miguel");
+    y.appendChild(t);
+    var z = document.getElementsByClassName("f2 f1-l b di lh-title mb3 white mw6 bg-primary")[0];
+    z.appendChild(y);
+  }
 }
-if (location.pathname = "/") {window.onload = customizeTitle;}
+window.onload = customizeTitle;
 
