9bc0170013c12405f254e5b2a73ada3325c913e8
Victorcasas <Victorcasas@users.noreply.github.com>
2023-04-08 11:23:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+// Customize title
+function customizeTitle() {
+  var x = document.getElementsByTagName("H1")[0]; 
+  x.innerHTML = "El Blog de";  
+  var y = document.createElement("H1"); 
+  var t = document.createTextNode("Víctor Casas San Miguel");
+  y.appendChild(t);
+  var z = document.getElementsByClassName("f2 f1-l b di lh-title mb3 white mw6 bg-primary")[0];
+  z.appendChild(y);
+}
+window.onload = customizeTitle;
+
